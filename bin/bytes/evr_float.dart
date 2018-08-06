2a8275b7b36bf773501e6d14ec6335c3fd6e2e9a
Jim Philbin <jfphilbin@users.noreply.github.com>
2018-08-06 20:03:59 +0200

    Fixed bug with DcmDateTime.

--- bin/bytes/evr_float.dart
+++ bin/bytes/evr_float.dart
@@ -3,7 +3,7 @@
 // that can be found in the LICENSE file.
 // Original author: Jim Philbin <jfphilbin@gmail.edu> -
 // See the AUTHORS file for other contributors.
-
+//
 import 'dart:typed_data';
 
 import 'package:core/server.dart';
