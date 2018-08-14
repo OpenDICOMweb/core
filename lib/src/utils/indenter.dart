9a2207284b11adc03c1d8bbf275c28dc2b5f8de8
Jim Philbin <jfphilbin@users.noreply.github.com>
2018-08-14 15:17:26 +0200

    remove util/utils from directories in src/utils

--- lib/src/utils/indenter.dart
+++ lib/src/utils/indenter.dart
@@ -6,7 +6,6 @@
 //  Primary Author: Jim Philbin <jfphilbin@gmail.edu>
 //  See the AUTHORS file for other contributors.
 //
-
 export 'package:core/src/utils/indenter/indenter.dart';
 export 'package:core/src/utils/indenter/indenter_old_old.dart';
 export 'package:core/src/utils/indenter/prefixer.dart';
