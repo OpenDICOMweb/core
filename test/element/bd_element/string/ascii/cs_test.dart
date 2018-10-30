69d22c06c3f7e7fabd382c5e9c9cd270b90bedb0
binayakb <binayak.b@mwebware.com>
2018-10-30 18:52:10 +0530

    Enhanced tests in bd_element/string.

--- test/element/bd_element/string/ascii/cs_test.dart
+++ test/element/bd_element/string/ascii/cs_test.dart
@@ -66,6 +66,18 @@ void main() {
           final e1 = ByteElement.makeFromBytes(e0.bytes, rds, isEvr: true);
           log.debug('e1: $e1');
           expect(e0.hasValidValues, true);
+
+          expect(e0.code == e0.bytes.code, true);
+          expect(e0.eLength == e0.bytes.eLength, true);
+          expect(e0.vrCode == e0.bytes.vrCode, true);
+          expect(e0.vrIndex == e0.bytes.vrIndex, true);
+          expect(e0.vfLengthOffset == e0.bytes.vfLengthOffset, true);
+          expect(e0.vfLengthField == e0.bytes.vfLengthField, true);
+          expect(e0.vfLength == e0.bytes.vfLength, true);
+          expect(e0.vfOffset == e0.bytes.vfOffset, true);
+          expect(e0.vfBytes == e0.bytes.vfBytes, true);
+          expect(e0.vfBytesLast == e0.bytes.vfBytesLast, true);
+          expect(e0.hashCode == e0.bytes.hashCode, true);
         }
       }
     });
@@ -80,6 +92,18 @@ void main() {
           final e1 = ByteElement.makeFromBytes(e0.bytes, rds, isEvr: true);
           log.debug('e1: $e1');
           expect(e0.hasValidValues, true);
+
+          expect(e0.code == e0.bytes.code, true);
+          expect(e0.eLength == e0.bytes.eLength, true);
+          expect(e0.vrCode == e0.bytes.vrCode, true);
+          expect(e0.vrIndex == e0.bytes.vrIndex, true);
+          expect(e0.vfLengthOffset == e0.bytes.vfLengthOffset, true);
+          expect(e0.vfLengthField == e0.bytes.vfLengthField, true);
+          expect(e0.vfLength == e0.bytes.vfLength, true);
+          expect(e0.vfOffset == e0.bytes.vfOffset, true);
+          expect(e0.vfBytes == e0.bytes.vfBytes, true);
+          expect(e0.vfBytesLast == e0.bytes.vfBytesLast, true);
+          expect(e0.hashCode == e0.bytes.hashCode, true);
         }
       }
     });
@@ -94,6 +118,18 @@ void main() {
           final e1 = ByteElement.makeFromBytes(e0.bytes, rds, isEvr: true);
           log.debug('e1: $e1');
           expect(e0.hasValidValues, true);
+
+          expect(e0.code == e0.bytes.code, true);
+          expect(e0.eLength == e0.bytes.eLength, true);
+          expect(e0.vrCode == e0.bytes.vrCode, true);
+          expect(e0.vrIndex == e0.bytes.vrIndex, true);
+          expect(e0.vfLengthOffset == e0.bytes.vfLengthOffset, true);
+          expect(e0.vfLengthField == e0.bytes.vfLengthField, true);
+          expect(e0.vfLength == e0.bytes.vfLength, true);
+          expect(e0.vfOffset == e0.bytes.vfOffset, true);
+          expect(e0.vfBytes == e0.bytes.vfBytes, true);
+          expect(e0.vfBytesLast == e0.bytes.vfBytesLast, true);
+          expect(e0.hashCode == e0.bytes.hashCode, true);
         }
       }
     });
@@ -107,6 +143,18 @@ void main() {
           final e1 = ByteElement.makeFromBytes(e0.bytes, rds, isEvr: true);
           log.debug('e1: $e1');
           expect(e0.hasValidValues, true);
+
+          expect(e0.code == e0.bytes.code, true);
+          expect(e0.eLength == e0.bytes.eLength, true);
+          expect(e0.vrCode == e0.bytes.vrCode, true);
+          expect(e0.vrIndex == e0.bytes.vrIndex, true);
+          expect(e0.vfLengthOffset == e0.bytes.vfLengthOffset, true);
+          expect(e0.vfLengthField == e0.bytes.vfLengthField, true);
+          expect(e0.vfLength == e0.bytes.vfLength, true);
+          expect(e0.vfOffset == e0.bytes.vfOffset, true);
+          expect(e0.vfBytes == e0.bytes.vfBytes, true);
+          expect(e0.vfBytesLast == e0.bytes.vfBytesLast, true);
+          expect(e0.hashCode == e0.bytes.hashCode, true);
         }
       }
     });
@@ -121,6 +169,18 @@ void main() {
           final e1 = ByteElement.makeFromBytes(e0.bytes, rds, isEvr: true);
           log.debug('e1: $e1');
           expect(e0.hasValidValues, true);
+
+          expect(e0.code == e0.bytes.code, true);
+          expect(e0.eLength == e0.bytes.eLength, true);
+          expect(e0.vrCode == e0.bytes.vrCode, true);
+          expect(e0.vrIndex == e0.bytes.vrIndex, true);
+          expect(e0.vfLengthOffset == e0.bytes.vfLengthOffset, true);
+          expect(e0.vfLengthField == e0.bytes.vfLengthField, true);
+          expect(e0.vfLength == e0.bytes.vfLength, true);
+          expect(e0.vfOffset == e0.bytes.vfOffset, true);
+          expect(e0.vfBytes == e0.bytes.vfBytes, true);
+          expect(e0.vfBytesLast == e0.bytes.vfBytesLast, true);
+          expect(e0.hashCode == e0.bytes.hashCode, true);
         }
       }
     });
